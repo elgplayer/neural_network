07a55f60fa385a225ccdbe9edd4e039545659d55
Carl Elgcrona <carlval@hotmail.se>
2019-09-19 11:07:46 +0200

    Rewriting the entire code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  1 23:31:10 2018
+Created on Thu Sep 19 10:48:53 2019
 
-@author: Carl
+@author: carlelg
 """
 
 import os
@@ -11,117 +12,60 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-## Setup ##
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+# HYPER PARAMETERS
+digits = 10
+n_x = 784
+x_h = 64
+n_h = 64
+beta = 0.9
+lr = 0.5
+epochs = 50
 
 # Loads the data
 mnist_data = pickle.load(open('../data/mnist.pkl', 'rb'))
+x_train = mnist_data['training_images']
+y_train = mnist_data['training_labels']
+x_test  = mnist_data['test_images']
+y_test  = mnist_data['test_labels']
 
-
-# Activation functions
+# SGD
 def sigmoid(x):
-    return 1.0/(1.0 + np.exp(-x))
+    '''
+    Sigmoid
+    
+    Attributes:
+        * x (???)
+    '''
+    return 1.0/(1 + np.exp(-x))
 
-# Derative of the Sigmoid fucntion
 def sigmoid_derivative(x):
-    return sigmoid(x) * (1.0 - sigmoid(x))
+    '''
+    Sigmoid derivative
+    
+    Attributes:
+        * x (???)
+    '''
+    return x * (1.0 - x)
 
-# Plots the picture
-def picture_plot(input_data): 
+def MSE(predicted_val, true_val):
+    '''
+    Mean Error Squared
     
-    pixels = input_data.reshape((28, 28))
-    plt.imshow(pixels)
-
-
-# Params
-learning_rate = 0.01
-
-## Init ##
-
-# Weights  
-w2 = np.random.randn(16, 784)
-w3 = np.random.randn(16, 16)
-w4 = np.random.randn(10, 16)
-
-# Biases
-b2 = np.random.randn(16, 1)
-b3 = np.random.randn(16, 1)
-b4 = np.random.randn(10, 1)
-#%%
-
-# Loopy loop
-for k in range(1000):
+    Attributes:
+        * predicted_val (numpy array): Predicted value
+        * true_val (numpy array): Correct value
+    '''
+    return np.square(np.subtract(true_val,predicted_val)).mean() 
     
-    selected_data = k
-    input_data = mnist_data['training_images'][selected_data]
-    input_data = input_data.reshape(784, 1)
-    correct_answer = mnist_data['training_labels'][selected_data]
-    
-    #%% Feedforward 
-
-    z2 = np.matmul(w2, input_data) + b2
-    layer2 = sigmoid(z2)
-    
-    z3 = np.matmul(w3, layer2) + b3
-    layer3 = sigmoid(z3)
-    
-    z4 = np.matmul(w4, layer3) + b4
-    layer4 = sigmoid(z4)
-    
-    output = layer4
-
-    #%% Error
-    
-    cost = 0
-    
-    for i in range(len(output)):
-        
-        if i == correct_answer:
-            
-            cost += (output[i][0] - 1)**2
-        
-        else: 
-            
-            cost += (output[i][0] - 0)**2
-    
-    #%% Nabla C 
-    
-    nabla_c = np.zeros((10, 1))
-            
-    for i in range(len(output)):
-        
-        if i == correct_answer:
-            
-            nabla_c[i] = (1 - output[i][0])
-        
-        else: 
-            
-            nabla_c[i] = (0 - output[i][0])  
-    
-    #%% Output error 
-    
-    nabla_4 = np.multiply(nabla_c, sigmoid_derivative(z4))
-    
-    nabla_3 = np.multiply(np.matmul(w4.transpose(), nabla_4), sigmoid_derivative(z3))
-    
-    nabla_2 = np.multiply(np.matmul(w3.transpose(), nabla_3), sigmoid_derivative(z2))
-    
-    
-    #%% Gradient decent 
-    
-    # Weights update
-    w4 = w4 - learning_rate * np.matmul(nabla_4, layer3.transpose())
-    
-    w3 = w3 - learning_rate * np.matmul(nabla_3, layer2.transpose())
-    
-    w2 = w2 - learning_rate * np.matmul(nabla_2, input_data.transpose())
-    
-    # Biases update
-    b4 = b4 - learning_rate * nabla_4
-    
-    b3 = b3 - learning_rate * nabla_3
-    
-    b2 = b2 - learning_rate * nabla_2
-    
-    print(k, cost)
+# initialization
+params = {"W1": np.random.randn(n_h, n_x) * np.sqrt(1. / n_x),
+          "b1": np.zeros((n_h, 1)) * np.sqrt(1. / n_x),
+          "W2": np.random.randn(digits, n_h) * np.sqrt(1. / n_h),
+          "b2": np.zeros((digits, 1)) * np.sqrt(1. / n_h)}
 
 
