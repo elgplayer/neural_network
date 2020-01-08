156e7c0a1b334de3dc03c8c9e3061ca727ef3f0f
Carl Elg <carlval@hotmail.se>
2020-01-08 13:51:55 +0100

    Fixes?

--- src/conv_operations.jl
+++ src/conv_operations.jl
@@ -19,6 +19,13 @@ filter_matrix = [0 1; 2 3]
 input_matrix = [2 3 7 4 6 2 9; 6 6 9 8 7 4 3; 3 4 8 3 8 9 7; 7 8 3 6 6 3 4; 4 2 1 8 3 4 6; 3 2 4 1 9 8 3; 0 1 3 9 2 1 4]
 filter_matrix = [3 4 4; 1 0 2; -1 0 3]
 
+
+# input_matrix = [0 0 0 0 0 0 0; 0 1 0 0 0 1 0; 0 0 0 0 0 0 0; 0 0 0 1 0 0 0; 0 1 0 0 0 1 0; 0 0 1 1 1 0 0; 0 0 0 0 0 0 0]
+# filter_matrix = [0 0 1; 1 0 0; 0 1 1]
+
+input_matrix = [0 1 1 1 0 0 0; 0 0 1 1 1 0 0; 0 0 0 1 1 1 0; 0 0 0 1 1 0 0; 0 0 1 1 0 0 0 ; 0 1 1 0 0 0 0; 1 1 0 0 0 0 0]
+filter_matrix = [1 0 1; 0 1 0; 1 0 1]
+
 # input_matrix = [2 3 0 5 2.5 0; 2 1.5 0.5 0 7 0; 1.5 5 5 3 2 0; 3 5 7 1.5 0 0 ; 2 5 2 1.5 2 0; 0 0 0 0 0 0]
 # input_matrix = [2 3 0 5 2.5; 2 1.5 0.5 0 7; 1.5 5 5 3 2; 3 5 7 1.5 0 ; 2 5 2 1.5 2]
 
@@ -373,5 +380,5 @@ end
 
 
 # Shows the convuluted array
-smaller_array = striding(input_matrix, filter_matrix, 2)
+smaller_array = striding(input_matrix, filter_matrix, 1)
 show(stdout, "text/plain", smaller_array)
