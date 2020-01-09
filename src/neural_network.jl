a53dca42b23fbb34425a5e1ab6f3bc1a6e03708d
Carl Elg <carlval@hotmail.se>
2020-01-09 16:03:34 +0100

    MSE

---
+++ src/neural_network.jl
@@ -0,0 +1,27 @@
+
+# Y_𝒊 = [0, 0.2, 0.5, 0.9, 0, 0, 0, 0, 0.2, 0.53]
+# Ŷ__𝒊 = [0, 0.0, 0.0, 1, 0, 0, 0, 0, 0.0, 0.0]
+
+function MSE(Y_𝒊, Ŷ__𝒊)
+    """
+    Computes the MSE (Mean Squared Error)
+
+    Attributes:
+
+        * Y_𝒊 (Vector): Input Vector
+        * Ŷ__𝒊 (Vector): Vector of the correct output (hot encoding)
+
+    return: MSE sum
+    """
+
+    if length(Y_𝒊) == length(Ŷ__𝒊)
+
+        return sum((Y_𝒊 - Ŷ__𝒊) .^ 2)
+        
+    else
+
+        throw("DIMENSION_ERROR")
+
+    end
+
+end
