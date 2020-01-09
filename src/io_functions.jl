f4836e54d6b2e987ec0a6fe792f1a70f847e3b56
Carl Elg <carlval@hotmail.se>
2020-01-09 13:10:51 +0100

    Its now a module

--- src/io_functions.jl
+++ src/io_functions.jl
@@ -1,31 +1,41 @@
+module Data_Reading
 
 using CSV
-
-# Read the data
-const file_path = "data/mnist_test_small.csv"
-const data = CSV.read(file_path)
 
 # # Iterate over the rows
 # for i = 1:size(data)[1]
 #     #println(i)
 #     row = data[i, :]
 # end
-
 # https://www.juliabloggers.com/classifying-handwritten-digits-with-neural-networks/
 
-i = 1
-row = data[i, :]
-label = row.label
-picture_data = row[2:size(row)[1]]
+export read_dataset
 
-picture_data_matrix = Vector{Float64}()
-for i=1:size(picture_data)[1]
+function read_dataset()
 
-   append!(picture_data_matrix, picture_data[i])
+   # Read the data
+   file_path = "data/mnist_test_small.csv"
+   data = CSV.read(file_path)
+
+   i = 1
+   row = data[i, :]
+   label = row.label
+   picture_data = row[2:size(row)[1]]
+
+   picture_data_matrix = Vector{Float64}()
+   for i=1:size(picture_data)[1]
+
+      append!(picture_data_matrix, picture_data[i])
+
+   end
+
+   n_rows = 28
+   n_cols = 28
+   picture_data_matrix = reshape(picture_data_matrix, n_rows, n_cols)
+
+   return picture_data_matrix
 
 end
 
-const n_rows = 28
-const n_cols = 28
-picture_data_matrix = reshape(picture_data_matrix, n_rows, n_cols)
-println("END!")
+
+end
