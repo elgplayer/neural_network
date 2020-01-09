26ffa4362a4a759530ff41dea1842969e3b7377b
Carl Elg <carlval@hotmail.se>
2020-01-09 14:17:36 +0100

    Checks if the module is already loaded

--- main.jl
+++ main.jl
@@ -1,5 +1,12 @@
-include("src/io_functions.jl")
-using .Data_Reading
+
+# Check if the module is defined; if not --> Load the module
+if isdefined(Main, :Data_Reading) == false
+
+    println("Loading Data_Reading")
+    include("src/io_functions.jl")
+    using .Data_Reading
+
+end
 
 
 image = read_dataset()
