f6a7a74e6f45ddca8d4b903557621878e8da105d
Carl Elg <carlval@hotmail.se>
2020-01-09 12:42:10 +0100

    Fixes?

--- src/io_functions.jl
+++ src/io_functions.jl
@@ -1,17 +1,33 @@
 
 using CSV
-using PyPlot
+#using PyPlot
 
-file_path = "data/mnist_test_small.csv"
-data = CSV.read(file_path)
+function dafaq()
+   file_path = "data/mnist_test_small.csv"
+   data = CSV.read(file_path)
 
-# # Iterate over the rows
-# for i = 1:size(data)[1]
-#     #println(i)
-#     row = data[i, :]
-# end
+   # # Iterate over the rows
+   # for i = 1:size(data)[1]
+   #     #println(i)
+   #     row = data[i, :]
+   # end
 
-i = 1
-row = data[i, :]
-label = row.label
-picture_data = row[2:size(row)[1]]
+   # https://www.juliabloggers.com/classifying-handwritten-digits-with-neural-networks/
+
+   i = 1
+   row = data[i, :]
+   label = row.label
+   picture_data = row[2:size(row)[1]]
+
+   picture_data_matrix = Vector{Float64}()
+   for i=1:size(picture_data)[1]
+
+      append!(picture_data_matrix, picture_data[i])
+
+   end
+
+   n_rows = 28
+   n_cols = 28
+   picture_data_matrix = reshape(picture_data_matrix, n_rows, n_cols)
+   
+end
