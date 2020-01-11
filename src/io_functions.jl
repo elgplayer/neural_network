dc5f8a314f9b61102a7041d8c74524c64fd0510c
Carl Elg <carlval@hotmail.se>
2020-01-11 17:07:27 +0100

    Better data set_reader

--- src/io_functions.jl
+++ src/io_functions.jl
@@ -5,7 +5,7 @@ __precompile__()
 #module Data_Reading
 
 using CSV
-
+using DataFrames
 
 # # Iterate over the rows
 # for i = 1:size(data)[1]
@@ -19,7 +19,8 @@ using CSV
 function read_dataset(row=1, data_reshape=false)
 
    # Read the data
-   file_path = "data/mnist_test_small.csv"
+   #file_path = "data/mnist_test_small.csv"
+   file_path = "data/mnist_train.csv"
    data = CSV.read(file_path)
 
    i = row
@@ -53,6 +54,44 @@ function read_dataset(row=1, data_reshape=false)
    return (label, picture_data_matrix)
 
 end
+
+
+function read_dataset_2()
+    
+   file_path = "data/mnist_train.csv"
+   data = CSV.read(file_path)
+
+   # Convert the dataframe to an array
+   df1 = convert(Matrix,data)
+
+   # Remove the first column
+   image_array = df1[:, 1:size(df1,2) .!= 1] # Removes the first column
+   answer_array = df1[:, 1] # Selects the first column
+
+   image_list = [];
+   n_rows = 28
+   n_cols = 28
+
+   for i=1:size(answer_array)[1]
+
+       image = image_array[i, :] # Select the entire row at index "i"
+       image = reshape(image, n_rows, n_cols) # Reshape to a 28x28 array
+       image = rotl90(image) # Rotate the image left 90 degress
+       image = reshape(image, :, 1) # Convert back to a vector
+       answer = answer_array[i] # Select answer
+
+       # Create tuple
+       image_item = (answer, image)
+
+       # Push the item to the list
+       push!(image_list, image_item)
+
+   end
+
+   return image_list
+
+end
+
 
 
 #end
