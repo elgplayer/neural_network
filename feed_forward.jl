c150a37a4641388ddb4f87eeddfffebc0629bc7a
Carl Elg <carlval@hotmail.se>
2020-01-12 13:54:06 +0100

    Splitting of files so that the feedforward actually works

---
+++ feed_forward.jl
@@ -0,0 +1,154 @@
+using Revise
+using LinearAlgebra
+using Distributions
+
+
+include("src/io_functions.jl")
+include("src/activation_functions.jl")
+include("src/neural_network.jl")
+
+
+const n_x = 784 # Image as 1d vector
+const n_h = 64 # Hidden layer size
+const output_size = 10 # Number of output nodes
+η = 1 # Learning rate
+epoches = 4 # Number of training iteration
+
+
+# Init the weights
+let 
+
+    # Generate the weights and biases with a gausian distribution
+    μ = 0 # The mean of the truncated Normal
+    _σ = 1  # The standard deviation of the truncated Normal
+
+    # Init the weights
+    w1 = rand(Truncated(Normal(μ, _σ), -1, 1), n_h, n_x)
+    w2 = rand(Truncated(Normal(μ, _σ), -1, 1), n_h, n_h)
+    w3 = rand(Truncated(Normal(μ, _σ), -1, 1), output_size, n_h)
+
+    # Init the biases
+    b1 = rand(Truncated(Normal(μ, _σ), -1, 1), n_h, 1)
+    b2 = rand(Truncated(Normal(μ, _σ), -1, 1), n_h, 1)
+
+    # Load the test and training data
+    global train_data = read_dataset("mnist_train")
+    global test_data = read_dataset("mnist_test")
+
+    # Training variables
+    old_cost = 0
+    global correct_predictions = 0
+    global cost_arr = []
+    global prediction_arr = []
+    global wrong_predictions = []
+
+    activation_func="sigmoid"
+    derative=true
+
+    println("-- Starting --")
+
+    for epoch=1:epoches
+
+        if epoch == epoches
+            save_fails = true
+        else
+            save_fails = false
+        end
+
+        # Training
+        for i=1:size(train_data)[1]
+
+            # Get the data
+            data = train_data[i]
+            label = data[1]
+            image = data[2]
+
+            # Normalisze the data
+            image = image / 255
+            x = image
+
+            # Feedforward
+            z1 = (w1 * x) .+ b1
+            a1 = activation_function(z1, activation_func)
+
+            z2 = (w2 * a1) .+ b2
+            a2 = activation_function(z2, activation_func)
+
+            z3 = (w3 * a2)
+            a3 = activation_function(z3, activation_func)
+
+            # Make the label one hot encoded
+            desired_output = one_hot(label)
+
+            # Calculate the cost
+            cost = MSE(a3, desired_output)
+
+            # Back propigation
+            # Error in output layer
+            ∇a_C_3 = cost_derivative(a3, desired_output)
+            δ_3 = hadmard(∇a_C_3, activation_function(z3, activation_func, derative))
+
+            # Error in second layer
+            ∇a_C_2 = (transpose(w3) * δ_3)  
+            δ_2 = hadmard(∇a_C_2, activation_function(z2, activation_func, derative))
+
+            # Error in first layer
+            ∇a_C_1 = (transpose(w2) * δ_2)  
+            δ_1 = hadmard(∇a_C_1, activation_function(z1, activation_func, derative))
+
+
+            # Gradient descent
+            # Third layer
+            w3 = w3 .- η * δ_3 * transpose(a2)
+
+            # Second layer
+            w2 = w2 .- η * δ_2 * transpose(a1)
+            b2 = b2 .- η * δ_2
+
+            # First layer
+            w1 = w1 .- η * δ_1 * transpose(x)
+            b1 = b1 .- η * δ_1
+
+            if i % 10000 == 0
+                println("Epoch : ", epoch, " | I: ", i)
+            end
+
+        end
+
+    
+        # Test
+        for i=1:size(test_data)[1]
+
+            # Get the data
+            data = test_data[i]
+            label = data[1]
+            image = data[2]
+
+            # Normalisze the data
+            image = image / 255
+            x = image
+
+            # Feedforward
+            z1 = (w1 * x) .+ b1
+            a1 = activation_function(z1, activation_func)
+
+            z2 = (w2 * a1) .+ b2
+            a2 = activation_function(z2, activation_func)
+
+            z3 = (w3 * a2)
+            a3 = activation_function(z3, activation_func)
+
+            # Checks the prediction
+            correct_predictions += check_prediction(a3, label, save_fails, image)
+            
+        end
+
+
+        
+        println("Epoch: ", epoch, " | Number of correct_predictions: ", correct_predictions)
+        append!(prediction_arr, correct_predictions)
+        correct_predictions = 0
+
+    end
+
+end
--- src/conv_operations.jl
+++ src/conv_operations.jl
@@ -1,34 +1,10 @@
 
 import Statistics
 
-#input_matrix = [2 0 1 1; 0 1 0 0; 0 0 1 0; 0 3 0 0]
-#filter_matrix = [1 0 1; 0 0 0; 0 1 0]
-
-# Striding: https://d2l.ai/chapter_convolutional-neural-networks/padding-and-strides.html
-# Adding the result to a new matrix: https://stackoverflow.com/questions/29897498/how-to-construct-matrix-from-row-column-vectors-in-julia
-
-# input_matrix = [0 0 0 0 0; 0 0 3 6 0; 0 1 4 7 0; 0 2 5 8 0; 0 0 0 0 0]
-# filter_matrix = [0 2; 1 3]
-
-input_matrix = [0 0 0 0 0; 0 0 3 6 0; 0 1 4 7 0; 0 2 5 8 0; 0 0 0 0 0]
-
-
-input_matrix = [0 0 0 0 0; 0 0 1 2 0; 0 3 4 5 0; 0 6 7 8 0; 0 0 0 0 0]
-filter_matrix = [0 1; 2 3]
- 
-input_matrix = [2 3 7 4 6 2 9; 6 6 9 8 7 4 3; 3 4 8 3 8 9 7; 7 8 3 6 6 3 4; 4 2 1 8 3 4 6; 3 2 4 1 9 8 3; 0 1 3 9 2 1 4]
-filter_matrix = [3 4 4; 1 0 2; -1 0 3]
-
-
-# input_matrix = [0 0 0 0 0 0 0; 0 1 0 0 0 1 0; 0 0 0 0 0 0 0; 0 0 0 1 0 0 0; 0 1 0 0 0 1 0; 0 0 1 1 1 0 0; 0 0 0 0 0 0 0]
-# filter_matrix = [0 0 1; 1 0 0; 0 1 1]
-
 input_matrix = [0 1 1 1 0 0 0; 0 0 1 1 1 0 0; 0 0 0 1 1 1 0; 0 0 0 1 1 0 0; 0 0 1 1 0 0 0 ; 0 1 1 0 0 0 0; 1 1 0 0 0 0 0]
 filter_matrix = [1 0 1; 0 1 0; 1 0 1]
 
-# input_matrix = [2 3 0 5 2.5 0; 2 1.5 0.5 0 7 0; 1.5 5 5 3 2 0; 3 5 7 1.5 0 0 ; 2 5 2 1.5 2 0; 0 0 0 0 0 0]
-# input_matrix = [2 3 0 5 2.5; 2 1.5 0.5 0 7; 1.5 5 5 3 2; 3 5 7 1.5 0 ; 2 5 2 1.5 2]
-
+# https://towardsdatascience.com/training-a-convolutional-neural-network-from-scratch-2235c2a25754
 
 # Todo: Maybe support diffrent stride for X and Y
 function striding(input_matrix, filter_arr, step_size=1)
